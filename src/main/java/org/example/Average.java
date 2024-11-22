8f240b1b85d093f47414a161e92e484d7b3d2b65
waffleprinter <dwamsie@gmail.com>
2024-11-21 22:31:00 -0500

    added comments

--- src/main/java/org/example/Average.java
+++ src/main/java/org/example/Average.java
@@ -3,15 +3,23 @@ package org.example;
 
 import java.util.Scanner;
 
+/**
+ This class contains an array of integers and its mean.
+ */
 public class Average {
     private int[] data = new int[5];
     private double mean;
 
+    /**
+     Constructor
+     Asks the user for 5 integers and places them into the data array.
+     Then, it sorts the data using selection sort descending and calculates the mean.
+     */
     Average() {
         Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < data.length; i++) {
-            System.out.printf("Please enter score %d: ", i + 1);
+            System.out.printf("Please enter score %d: ", i + 1); // Humans start counting with 1 instead of 0
             int userNumber = scanner.nextInt();
             data[i] = userNumber;
         }
@@ -22,6 +30,10 @@ public class Average {
         selectionSort();
     }
 
+    /**
+     The calculateMean method
+     Calculates the mean based on the values in data.
+     */
     public void calculateMean() {
         double runningTotal = 0;
 
@@ -32,6 +44,10 @@ public class Average {
         mean = runningTotal / data.length;
     }
 
+    /**
+     The toString method
+     @return a string with the values stored in data and the mean.
+     */
     public String toString() {
         String string = "Values: ";
 
@@ -44,6 +60,10 @@ public class Average {
         return string;
     }
 
+    /**
+     The selectionSort method
+     Uses selection sort to sort the values in data from largest to smallest.
+     */
     public void selectionSort() {
         for (int i = 0; i < data.length - 1; i++) {
             int max = i;
