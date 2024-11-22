153215912bba0aacfae76c6babe9b71eb93f9743
waffleprinter <dwamsie@gmail.com>
2024-11-21 22:34:41 -0500

    Initial commit

---
+++ src/main/java/org/example/AverageDriver.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ This class is to debug the Average class.
+ */
+public class AverageDriver {
+    public static void main(String[] args) {
+        Average average = new Average();
+        System.out.println(average.toString());
+    }
+}
