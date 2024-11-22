f6025b6098e1962e6e710ca4d62b9dafee9b5656
waffleprinter <dwamsie@gmail.com>
2024-11-21 22:25:56 -0500

    Added constructor implementation, toString() implementation, selectionSort() implementation

--- src/main/java/org/example/Average.java
+++ src/main/java/org/example/Average.java
@@ -1,11 +1,25 @@
 package org.example;
 
+
+import java.util.Scanner;
+
 public class Average {
-    private int[] data;
+    private int[] data = new int[5];
     private double mean;
 
     Average() {
+        Scanner scanner = new Scanner(System.in);
 
+        for (int i = 0; i < data.length; i++) {
+            System.out.printf("Please enter score %d: ", i + 1);
+            int userNumber = scanner.nextInt();
+            data[i] = userNumber;
+        }
+
+        scanner.close();
+
+        calculateMean();
+        selectionSort();
     }
 
     public void calculateMean() {
@@ -19,10 +33,30 @@ public class Average {
     }
 
     public String toString() {
+        String string = "Values: ";
 
+        for (int value : data) {
+            string += value + " ";
+        }
+
+        string += "\nMean: " + mean;
+
+        return string;
     }
 
     public void selectionSort() {
+        for (int i = 0; i < data.length - 1; i++) {
+            int max = i;
 
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[j] > data[max]) {
+                    max = j;
+                }
+            }
+
+            int temp = data[i];
+            data[i] = data[max];
+            data[max] = temp;
+        }
     }
 }
