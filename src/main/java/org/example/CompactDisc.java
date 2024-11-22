91ed562058c62111b18970afc7d943c89975fc6a
waffleprinter <dwamsie@gmail.com>
2024-11-21 22:40:24 -0500

    uncommented code and added task 3 requirements

--- src/main/java/org/example/CompactDisc.java
+++ src/main/java/org/example/CompactDisc.java
@@ -1,36 +1,34 @@
-//package org.example;
-//
-//import java.util.Scanner;
-//import java.io.*;
-//
-///**
-// This program creates a list of songs for a CD
-// by reading from a file.
-// */
-//public class CompactDisc
-//{
-//    public static void main(String[] args)throws IOException
-//    {
-//        Scanner input = new Scanner(new File("Classics.txt"));
-//        String title;
-//        String artist;
-//
-//        // ADD LINES FOR TASK #3 HERE
-//        // Declare an array of Song objects, called cd, with a size of 6
-//        for (int i = 0; i < cd.length; i++)
-//        {
-//            title = input.nextLine();
-//            artist = input.nextLine();
-//
-//            // ADD LINES FOR TASK #3 HERE
-//            // Fill the array by creating a new song with the title and artist and storing it in the appropriate
-//            // position in the array
-//        }
-//        System.out.println("Contents of Classics:");
-//        for (int i = 0; i < cd.length; i++)
-//        {
-//            // ADD LINES FOR TASK #3 HERE
-//            // Print the contents of the array to the console
-//        }
-//    }
-//}
+package org.example;
+
+import java.util.Scanner;
+import java.io.*;
+
+/**
+ This program creates a list of songs for a CD
+ by reading from a file.
+ */
+public class CompactDisc
+{
+    public static void main(String[] args)throws IOException
+    {
+        Scanner input = new Scanner(new File("src/main/Classics.txt"));
+        String title;
+        String artist;
+
+        Song[] cd = new Song[6];
+
+        for (int i = 0; i < cd.length; i++)
+        {
+            title = input.nextLine();
+            artist = input.nextLine();
+
+            cd[i] = new Song(title, artist);
+        }
+
+        System.out.println("Contents of Classics:");
+        for (int i = 0; i < cd.length; i++)
+        {
+            System.out.print(cd[i].toString());
+        }
+    }
+}
